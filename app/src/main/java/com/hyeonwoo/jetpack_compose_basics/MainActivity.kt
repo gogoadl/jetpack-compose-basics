ea7803db851bd9b258acf5e8c08568c37097e12e
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-05-25 18:34:43 +0900

    Update MainActivity.kt

--- app/src/main/java/com/hyeonwoo/jetpack_compose_basics/MainActivity.kt
+++ app/src/main/java/com/hyeonwoo/jetpack_compose_basics/MainActivity.kt
@@ -3,9 +3,15 @@ package com.hyeonwoo.jetpack_compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -50,17 +56,9 @@ fun OnboardingScreen(
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    JetpackcomposebasicsTheme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
-
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if (shouldShowOnBoarding) {
@@ -70,23 +68,14 @@ fun MyApp(modifier: Modifier = Modifier) {
     }
 
 }
-
-@Preview
-@Composable
-private fun MyAppPreview() {
-    JetpackcomposebasicsTheme {
-        MyApp(Modifier.fillMaxSize())
-    }
-}
-
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) {
+            name -> Greeting(name = name)
         }
     }
 }
@@ -94,7 +83,10 @@ private fun Greetings(
 @Composable
 private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(if (expanded.value) 48.dp else 0.dp,
+    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+    stiffness = Spring.StiffnessLow)
+    )
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -102,7 +94,7 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
@@ -110,6 +102,22 @@ private fun Greeting(name: String) {
                 Text(if (expanded.value) "Show less" else "Show more")
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackcomposebasicsTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview
+@Composable
+private fun MyAppPreview() {
+    JetpackcomposebasicsTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
